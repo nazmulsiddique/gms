b275418d4b44db8d1d2885b934fe27b9dfd19895
Md. Nazmul Siddique <27219@wgbd.com>
2024-03-29 22:33:30 +0600

    home page almost done

--- js/script.js
+++ js/script.js
@@ -57,3 +57,43 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
 });
+
+$(document).ready(function () {
+    $('#owl-carousel-1').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 4
+          },
+          1000: {
+            items: 5
+          }
+        }
+      });
+    $('#owl-carousel-2').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 4
+          }
+        }
+      });
+
+});
